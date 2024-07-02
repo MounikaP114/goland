549b222bc9c39e2ec621f35fa9948dfc67609aa4
Mounika P <mounikapotladurthiofficial@gmail.com>
2024-07-02 10:05:04 +0530

    bug fix

--- client/src/pages/Properties.jsx
+++ client/src/pages/Properties.jsx
@@ -106,8 +106,7 @@ export default function Properties() {
           )}
           <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
             <p className="text-2xl font-semibold">
-              {listing.name} - ₹{" "}
-              {listing.offer ? listing.discountPrice : listing.regularPrice}
+              {listing.name} - ₹ {listing.regularPrice}
               {listing.type === "rent" && " / month"}
             </p>
 
@@ -125,7 +124,7 @@ export default function Properties() {
 
               {listing.offer && (
                 <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
-                  ₹{+listing.regularPrice - listing.discountPrice} OFF
+                  ₹{+listing.discountPrice} OFF
                 </p>
               )}
             </div>
